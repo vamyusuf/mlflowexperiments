0047049433844a3e270169b19f677be7a6e82ae6
vamyusuf <ibnezzynyoussef@gmail.com>
2025-03-04 14:07:03 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## MLFLOW experiments
+
+
+
--- app.py
+++ app.py
@@ -17,6 +17,10 @@ import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
 
+import dagshub
+
+dagshub.init(repo_owner='vamyusuf', repo_name='mlflowexperiments', mlflow=True)
+
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
@@ -77,6 +81,8 @@ if __name__ == "__main__":
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
 
+        remote_server_uri="https://dagshub.com/vamyusuf/mlflowexperiments.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
